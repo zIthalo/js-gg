7cbf9c8c97abcfb7ae44f5de071055f76e13fcc6
zIthalo <ithalowillian@gmail.com>
2022-09-01 22:14:01 -0300

    novinho

--- modulo-d/ex009.js
+++ modulo-d/ex009.js
@@ -1,7 +1,8 @@
 var país = 'EUA'
-console.log(`Vivendo em ${país}`)
+console.log(`Nascido no(s) ${país}`)
 if (país == 'Brasil'){
     console.log ('Você é brasileiro')
 } else{
     console.log('Você é estrangeiro.')
 }
+// instale a extensão node.js exec e perte F8
--- modulo-d/ifelse1.js
+++ modulo-d/ifelse1.js
@@ -1,8 +1,9 @@
-var vel = 50.5
-console.log(`a velocidade do seu carro ${vel} por hora.`)
+var vel = 57.09
+    console.log(`a velocidade do seu carro ${vel}km/h.`)
 if (vel >= 60){
     console.log(`Você ultrapassou a velocidade máxima. MULTADO!!`)
 }else{
     console.log(`De boa!`)
 }
 console.log(`Dirija sempre usando o sinto de segurança.`)
+// instale a extensão node.js exec e perte F8
